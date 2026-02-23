ff03f24d70928db05508f3515393cc9f6011e48c
Ibrahim Bello <ibrbello10@gmail.com>
2026-02-22 17:20:30 -0800

    progress

--- main.cpp
+++ main.cpp
@@ -2,61 +2,68 @@
 // This program uses a dynamic array of structs to store a 
 // directory of employees at a company.
 
+// Employee data: Name, role, salary, vacation days used in the last 4 months
+
 #include <iostream>
+#include <string>
 using namespace std;
 
-const int NR_GRADES = 2, NR_STUDENTS = 3;
+const int NR_MONTHS = 4, NR_EMPLOYEES = 3;
 
-struct Student {
+struct Employee {
     string name;
-    int id;
-    int * grades;
+    string role; // Job title 
+    int salary; // Yearly salary
+    int * vacayDays; // Number of vacation days used in each of the last NR_MONTHS months
 
-    ~Student() {
-        if (grades) 
-            delete [] grades;
-        grades = nullptr;
+    ~Employee() {
+        if (vacayDays) 
+            delete [] vacayDays;
+        vacayDays = nullptr;
     }
 };
 
-void inputStudent(Student *);
-void displayStudent(Student *);
+void inputEmployee(Employee *);
+void displayEmployee(Employee *);
 
 int main() {
-    Student *roster = new Student[NR_STUDENTS];
+    Employee *team = new Employee[NR_EMPLOYEES];
     
-    for (int i = 0; i < NR_STUDENTS; i++)
-        inputStudent(&roster[i]);
+    for (int i = 0; i < NR_EMPLOYEES; i++)
+        inputEmployee(&team[i]);
     
-    for (int i = 0; i < NR_STUDENTS; i++)
-        displayStudent(&roster[i]);
+    for (int i = 0; i < NR_EMPLOYEES; i++)
+        displayEmployee(&team[i]);
     
     return 0;
 }
 
-void inputStudent(Student * sptr) {
-    static int nrStu = 1;
-    cout << "Input data for Student #" << nrStu << ":\n";
+void inputEmployee(Employee * eptr) {
+    static int nrEmp = 1;
+    cout << "Input data for Employee #" << nrEmp << ":\n";
     cout << "Name: ";
-    getline(cin, sptr->name);
-    cout << "ID: ";
-    cin >> sptr->id;
-    sptr -> grades = new int[NR_GRADES];
-    for (int i = 0; i < NR_GRADES; i++) {
-        cout << "Grade #" << i + 1 << ": ";
-        cin >> sptr->grades[i];
+    getline(cin, eptr->name);
+    cout << "Job Title: ";
+    getline(cin, eptr->role);
+    cout << "Salary: ";
+    cin >> eptr->salary;
+    eptr -> vacayDays = new int[NR_MONTHS];
+    for (int i = 0; i < NR_MONTHS; i++) {
+        cout << "Vacation days used in month #" << i + 1 << ": ";
+        cin >> eptr->vacayDays[i];
     }
     cin.ignore();
     cout << endl << endl;
-    nrStu++;
+    nrEmp++;
 }
 
-void displayStudent(Student * sptr) {
-    cout << "Student summary:\n";
-    cout << "Name: " << sptr->name << endl;
-    cout << "ID: " << sptr->id << endl;
-    for (int i = 0; i < NR_GRADES; i++)
+void displayEmployee(Employee * eptr) {
+    cout << "Employee summary:\n";
+    cout << "Name: " << eptr->name << endl;
+    cout << "Job Title: " << eptr->role << endl;
+    cout << "Salary: " << eptr->salary << endl;
+    for (int i = 0; i < NR_MONTHS; i++)
         cout << "Grade #" << i + 1 << ": " 
-             << sptr->grades[i] << endl;
+             << eptr->vacayDays[i] << endl;
     cout << endl << endl;
 }
